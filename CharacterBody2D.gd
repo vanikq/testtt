fe3e4e552ac6de5a2b229d955cb402bd82f4d34f
vanikq <girjevivan2010@gmail.com>
2024-07-29 20:49:09 +0300

    top

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
+func _physics_process(_delta):
 
 	var direction = Input.get_vector("a", "d", "w", "s")
 	if direction:
