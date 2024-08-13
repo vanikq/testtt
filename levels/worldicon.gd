d036baa2d140b0cd6854df5db4fd33edd0c99739
vanikq <girjevivan2010@gmail.com>
2024-08-13 14:01:38 +0300

    huinea

---
+++ levels/worldicon.gd
@@ -0,0 +1,9 @@
+@tool
+extends Control
+class_name  worldicon
+
+@export var level_name := "1"
+@export var next_level_up: LevelIcon
+@export var next_level_down: LevelIcon
+@export var next_level_left: LevelIcon
+@export var next_level_right: LevelIcon
